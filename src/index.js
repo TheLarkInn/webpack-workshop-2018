18b7e86d5686964ef5ed764fce7d0f3ca682b1a7
Sean Larkin <sean.larkin@cuw.edu>
2018-04-06 12:27:13 -0700

    feat(): add code splitting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import foo from "./foo";
 import {a} from "./bar";
 import imgSrc from "./icon-square-small.jpg";
 import "./index.css";
-
-console.log(imgSrc)
+// import foo from "./foo";
+const foo = () => import("./foo")
+    .then(m => m.default);
 
 const button = document.createElement("button");
 button.innerText = "BUTTON";
-debugger
 const fancyImage = document.createElement("img");
 fancyImage.src = imgSrc;
 
 document.body.appendChild(button);
 document.body.appendChild(fancyImage);
+
+button.addEventListener("click", e => {
+    foo().then(module => {
+        debugger
+
+    });
+
+    import("date-fns").then(m => {
+        const today = new Date();
+        const tomorrow = m.addDays(today, 1)
+        console.log(tomorrow);
+    });
+});
+
+const select = document.createElement('div');
+
+select.innerHTML = `<select>
+  <option value=""></option>
+  <option value="a">A Theme</option>
+  <option value="b">B Theme</option>
+</select>`;
+
+document.body.appendChild(select);
+
+const getTheme = (themeName) => import(`./themes/${themeName}.js`); 
+
+select.firstElementChild.addEventListener('change', e => {
+    const value = e.target.value;
+    getTheme(value).then(themeModule => {
+        debugger;
+        themeModule.default();
+    })
+});
