c142c0019f8556cc7a657f2004aab375fee444fc
Sean Larkin <sean.larkin@cuw.edu>
2018-04-16 14:02:20 -0700

    feat(common): add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,13 @@ const webpackMerge = require("webpack-merge");
 const loadPresets = require("./build-utils/loadPresets");
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = ({ mode, presets } = { mode: "production", presets: [] }) =>
   webpackMerge(
     {
       mode,
-      plugins: [new webpack.ProgressPlugin()]
+      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
     },
     modeConfig(mode),
     loadPresets({ mode, presets })
