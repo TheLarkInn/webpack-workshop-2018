f3d45681fba32a624e9ca19546b3955074845664
Sean Larkin <sean.larkin@cuw.edu>
2018-05-08 15:30:37 -0500

    feat(04011)

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,5 +1,16 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = () => ({
   output: {
-    filename: "[chunkhash].js"
-  }
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()]
 });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 
+import css from "./footer.css";
+import buttonStyles from "./button.css";
+
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
 document.body.appendChild(button);
