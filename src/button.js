35608c6bb5504aa9de7b46ada9fbf2c12a847ded
Sean Larkin <sean.larkin@cuw.edu>
2018-05-08 11:53:59 -0500

    031 all module types

---
+++ src/button.js
@@ -0,0 +1,11 @@
+// take a str, the button label and return a element
+/**
+ *
+ * @param {string} buttonName
+ * @returns {Element}
+ */
+const makeButton = buttonName => {
+  return `Button: ${buttonName}`;
+};
+
+module.exports = makeButton;
