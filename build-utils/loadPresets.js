4de3c04d0ffcb7bb44741ff3fb6eabc1c606df4b
Sean Larkin <sean.larkin@cuw.edu>
2018-05-08 16:07:50 -0500

    feat(04013):

---
+++ build-utils/loadPresets.js
@@ -0,0 +1,14 @@
+const webpackMerge = require("webpack-merge");
+
+const applyPresets = env => {
+  const { presets } = env;
+  /** @type {string[]} */
+  const mergedPresets = [].concat(...[presets]);
+  const mergedConfigs = mergedPresets.map(presetName =>
+    require(`./presets/webpack.${presetName}`)(env)
+  );
+
+  return webpackMerge({}, ...mergedConfigs);
+};
+
+module.exports = applyPresets;
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpackMerge = require("webpack-merge");
 
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+const presetConfig = require("./build-utils/loadPresets");
 
 module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
   return webpackMerge(
@@ -28,6 +29,7 @@ module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
       },
       plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
     },
-    modeConfig(mode)
+    modeConfig(mode),
+    presetConfig({ mode, presets })
   );
 };
