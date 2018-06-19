25b7bb93a1d315092c61f8602e40ca2660ecc9e7
Marc Grabanski <m@marcgrabanski.com>
2018-06-19 17:59:07 -0500

    add button.js code from the video

--- src/button.js
+++ src/button.js
@@ -5,7 +5,12 @@
  * @returns {Element}
  */
 const makeButton = buttonName => {
-  return `Button: ${buttonName}`;
+  const buttonLabel = `Button: ${buttonName}`;
+
+  document.createElement("button");
+  button.innerText = buttonLabel;
+
+  return button;
 };
 
 module.exports = makeButton;
