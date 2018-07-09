bb829479b30e4f61e5471106e475a76a1b5eb99b
Sean Larkin <sean.larkin@cuw.edu>
2018-07-09 14:39:05 +0100

    use webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpackMerge = require("webpack-merge");
+
+const modeConfig = env => require(`./build-utils/webpack.${env.mode}.js`)(env);
 
 module.exports = env => {
   console.log(env);
 
-  return {
-    mode: env.mode,
-    plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin()]
-  };
+  return webpackMerge(
+    {
+      mode: env.mode,
+      plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin()]
+    },
+    modeConfig(env)
+  );
 };
