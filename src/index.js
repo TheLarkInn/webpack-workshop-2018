1c12e0c7c5169becca1592f5fdb4eaedf013a443
Sean Larkin <sean.larkin@cuw.edu>
2018-07-10 11:16:58 +0100

    dynamic code splitting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./index.css";
 import createButton from "./button";
 // import colorElement from "./colorElement";
 import imgUrl from "./icon.jpeg";
+const setButtonColor = colorName => import(`./button-colors/${colorName}`);
 const getColorElement = () => import("./colorElement");
 
 console.log(imgUrl);
@@ -13,7 +14,10 @@ const div = document.createElement("div");
 div.innerText = "Hello World";
 div.style.color = "red";
 
+const buttonColorInput = document.createElement("input");
+
 const button = createButton("HEY FIRST BUTTON YO!");
+const button1 = createButton("CHANGE THIS THEME");
 
 button.addEventListener("click", e => {
   getColorElement().then(m => {
@@ -22,6 +26,16 @@ button.addEventListener("click", e => {
   });
 });
 
+button1.addEventListener("click", e => {
+  const colorVal = buttonColorInput.value;
+  setButtonColor(colorVal).then(({ color }) => {
+    debugger;
+    button1.style.color = color;
+  });
+});
+
 document.body.appendChild(button);
 document.body.appendChild(div);
 document.body.appendChild(img);
+document.body.appendChild(buttonColorInput);
+document.body.appendChild(button1);
