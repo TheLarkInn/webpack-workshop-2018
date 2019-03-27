126f425fed4db29b85b2fbb092fc561ad169f710
Sean Larkin <sean.larkin@cuw.edu>
2019-03-27 14:09:21 +0000

    004-sm1

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
+const webpack = require("webpack");
+const webpackMerge = require("webpack-merge")
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = ({mode}) => {
-  console.log(mode);
+const modeConfig = (env) => require(`./build-utils/webpack.${env.mode}.js`)(env)
 
-  return {
-    mode,
+module.exports = (env) => {
+  console.log(env.mode);
+
+  return webpackMerge({
+    mode: env.mode,
     plugins: [
-      new HtmlWebpackPlugin()
+      new HtmlWebpackPlugin(), new webpack.ProgressPlugin()
     ]
-  }
+  }, modeConfig(env))
 }
+
 
