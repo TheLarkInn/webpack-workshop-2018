45d00609b2302376e90d7f444d2bf60c014d0e05
Sean Larkin <sean.larkin@cuw.edu>
2019-03-28 11:12:20 +0000

    010-sm2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "./index.css";
 import "./button.css";
 import "./image.css";
-
 import imgSrc from "./webpack-image.png";
 import makeDiv from "./makeDiv";
 import makeButton from "./makeButton";
+const getMakeUniqSpan = () => import("./makeUniqSpan");
+
 
 const img = document.createElement("img");
 img.src = imgSrc;
@@ -12,7 +13,14 @@ img.src = imgSrc;
 const welcomeMessage = makeDiv("Welcome!", "blue");
 const button0 = makeButton("Click Me!");
 
-welcomeMessage.appendChild(img);
+button0.addEventListener("click", (e) => {
+    getMakeUniqSpan().then((m) => {
+        const makeUniqSpan = m.default;
+        const span0 = makeUniqSpan("my name is sean");
+        document.body.appendChild(span0);
+    })
+});
 
+welcomeMessage.appendChild(img);
 document.body.appendChild(welcomeMessage);
 document.body.appendChild(button0);
---
+++ src/makeUniqSpan.js
@@ -0,0 +1,11 @@
+
+
+import {uniq} from "lodash-es";
+
+export default (spanText) => {
+    const spanEl = document.createElement("span");
+    const uniqSpanText = uniq(spanText.split("")).join("");
+    spanEl.innerText = uniqSpanText;
+    
+    return spanEl;
+}
