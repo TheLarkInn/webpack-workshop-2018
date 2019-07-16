56461565e86d226a029a08f6937eb45ab29e42c2
Sean Larkin <sean.larkin@cuw.edu>
2019-07-16 14:44:30 +0200

    02

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import foo from "./foo";
 import {bar0, bar1, bar2} from "./bar";
 
 console.log(foo, bar0, bar1, bar2);
+
+const h2 = document.createElement("h2");
+h2.innerText = "Hello world!";
+
+document.body.appendChild(h2);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,14 @@
-module.exports = {
-    mode: "none"
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (env) => {
+    console.log(env);   
+
+    return {
+        mode: env.mode,
+        plugins: [
+            new webpack.ProgressPlugin(),
+            new HtmlWebpackPlugin()
+        ]
+    }
 }
