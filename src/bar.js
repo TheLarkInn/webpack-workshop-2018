de5c7a8de0e4f2f7bdbc7d4ab28f2699ddcb09f0
Sean Larkin <sean.larkin@cuw.edu>
2019-07-16 12:23:41 +0200

    01

---
+++ src/bar.js
@@ -0,0 +1,7 @@
+import baz from "./baz";
+
+const bar0 = "bar0";
+const bar1 = "bar1";
+const bar2 = baz;
+
+export {bar0, bar1, bar2}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import foo from "./foo";
+import {bar0, bar1, bar2} from "./bar";
+
+console.log(foo, bar0, bar1, bar2);
