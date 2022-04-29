45a49c1a2cd14ac2bcc6464a6b564d49fadda7bf
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-04-29 16:05:28 +0430

    add some plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = ({ mode }) => {
   return {
     mode,
     output: {
       filename: 'bundle.js',
     },
+    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
   };
 };
